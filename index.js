009b0dad7ee04bb275ba6c971f996cc282721f1c
lazikaz <souflakey@gmail.com>
2022-09-09 13:20:26 -0600

    testing deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+const {Sequelize} = require("sequelize")
+const { appendFile } = require("fs")
+const app = express()
+const sequelize = new Sequelize(process.env.DATABASE_URL, "garage_saling_db_user", "APzxGrZnvdTplTfKkktacdEsIPJk4SDT", {
+    dialect: "postgres",
+    dialectOptions: {
+        // ssl: {
+        //     rejectUnauthorized: false
+        // }
+    }
+})
+
+app.use(express.static(path.join(__dirname, "build")))
+app.use(cors())
+app.use(express.json())
+
+//PAGE CORRECTION
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+app.get("/c/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+//
